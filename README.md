b22362475f3cd51a82acab8466697e81986bcb28
NaimMhedhbi1 <naiim.mhedhbi@gmail.com>
2021-09-24 14:21:36 +0100

    "pytorch"

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Data-Science_2020_2022
+
+# remove space
+spaces = ['\u200b', '\u200e', '\u202a', '\u202c', '\ufeff', '\uf0d8', '\u2061', '\x10', '\x7f', '\x9d', '\xad', '\xa0']
+def remove_space(text):
+    """
+    remove extra spaces and ending space if any
+    """
+    for space in spaces:
+        text = text.replace(space, ' ')
+    text = text.strip()
+    text = re.sub('\s+', ' ', text)
+    return text 
+
--- google_colab_notes/notes_colab.py
+++ google_colab_notes/notes_colab.py
@@ -1,9 +1,39 @@
 #access to the driver 
-from google.colab import drive
-drive.mount('/content/drive',force_remount = True) 
 
+
+
+from google.colab import drive
 import pandas as pd
 import numpy as np
 import os
+#mount google colab
+drive.mount('/content/drive',force_remount = True) 
 #read the data
-df = pd.read_csv('/content/drive/MyDrive/datagram/train_data.csv')
+df = pd.read_csv('/content/drive/MyDrive/datagram/data/train_datam.csv')
+
+
+#version of tensorflow 
+import tensorflow as tf
+print(tf.__version__) 
+
+
+#save files from colab to google driver
+import pandas as pd
+train_df = pd.read_csv('/content/drive/MyDrive/datagram/data/train_df.csv')
+test_df = pd.read_csv('/content/drive/MyDrive/datagram/data/test_df.csv')
+
+train_df['category_id'] = train_df['category_id'].astype(int) 
+test_df['category_id'] = test_df['category_id'].astype(int)  
+
+train_df['pname'] = train_df['pname'].astype(str) 
+test_df['pname'] = test_df['pname'].astype(str)  
+
+train_df = train_df.groupby('category_id').filter(lambda x : (x['category_id'].count()>=1).any())
+test_df = test_df.groupby('category_id').filter(lambda x : (x['category_id'].count()>=1).any())
+
+
+train_df.to_csv('train_datam.csv')
+!cp train_datam.csv "/content/drive/MyDrive/datagram/data/"
+
+test_df.to_csv('test_datam.csv')
+!cp test_datam.csv "/content/drive/MyDrive/datagram/data/"
