883a82672f799abcd2d557a93b3eb15c77a6ba05
NaimMhedhbi1 <naiim.mhedhbi@gmail.com>
2021-09-24 15:32:42 +0100

    "added "

--- NLP/pytorch/load_custom_dataset.py
+++ NLP/pytorch/load_custom_dataset.py
@@ -2,11 +2,19 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torchtext.legacy import data
-TEXT = data.Field(sequential=True, tokenize=tokenize_input, lower=True)
-LABEL = data.Field(sequential=False, use_vocab=False)
 
 
-train,test = data.TabularDataset.splits(
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchtext.legacy import data
+
+train_data,test_data = data.TabularDataset.splits(
         path='/content/drive/MyDrive/datagram/data/', train='/content/drive/MyDrive/datagram/data/train_datam.csv'
         , test='/content/drive/MyDrive/datagram/data/test_datam.csv', format='csv',
         fields=[('pname', TEXT), ('category_id', LABEL)])
+
+#loading datsets
+train_data, valid_data=train_data.split(random_state=random.seed(999),split_ratio=0.8)
+device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+train_iter, valid_iter, test_iter = data.BucketIterator.splits((train_data, valid_data, test_data),batch_size = 64,device = device)
